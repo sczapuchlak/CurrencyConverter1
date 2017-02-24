edd325b1a8358e785e301a2e9da9f7883abc6cec
Stephanie <yourname@stephanie.com>
2017-02-23 20:47:01 -0600

    FIGURED IT OUT!!!!

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 // exchange rates
- var exchangeRates = {'EUR' : 0.94, 'JPY' : 112.86, 'IR': .015, 'EP': .063, 'BITCOIN': 1192.58};
+ var exchangeRates = { 'EUR' : 0.94, 'JPY' : 112.86, 'IR': .015, 'EP': .063, 'BITCOIN': 1192.58,'USD': 1.00};
 
 
  /* Handle GET request for home page*/
@@ -14,10 +14,12 @@ router.get('/', function(req, res){
 router.get('/convert', function(req,res){
     var dollars = req.query.dollar_amount;
     var convertTo = req.query.to_currency;
+    var convertFrom = req.query.from_currency;
     //res.send('To do: convert $' + dollars + ' to ' + convertTo);
-   var rate= exchangeRates[convertTo];
-    result =  dollars*rate;
-    result2 = result*rate;
-   res.render('results', {dollars: dollars, result: result2, currency: convertTo})
+   var rateTwo= exchangeRates[convertTo];
+   var rate = exchangeRates[convertFrom];
+    USCurrency =  dollars*rate;
+    result2 = USCurrency/rateTwo;
+   res.render('results', {dollars: dollars, UsCurrency: USCurrency, result2: result2, currency: convertFrom})
 });
     module.exports=router;
