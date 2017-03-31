f4ac4ed05e44757d65bfb867f62b8f02aff232a3
sczapuchlak <[sczapuchlak@gmail.com]>
2017-03-30 23:18:51 -0500

    Says my function wont work :(

--- helpers/oer.js
+++ helpers/oer.js
@@ -6,33 +6,33 @@ var baseURL = 'https://openexchangerates.org/api/latest.json/';
 // Makes requests to the OER service using request.
 // A callback checks for errors and then calls a method to
 // process the JSON and return a page to the client.
-function OERRequest(callback, today){
-    var queryParam = {};
+function OERRequest(callback) {
+    var queryParam = {'app_id': APIKEY};
     var APIKEY = process.env.OER_API_KEY;
     console.log(process.env.OER_API_KEY);
-}
+
 
 //Use request module to request data from OER service.
 //Must handle result in callback.
-request( {uri :baseURL, qs: queryParam} , function(error, oer_response, body){
+    request({uri: baseURL, qs: queryParam}, function (error, oer_response, body) {
 
-    if (!error && oer_response.statusCode == 200){
-        //No error, and there is a response from OER. Expect the response to be a string.
-        //console.log("NASA SAYS \n" + JSON.stringify(body));
-        var oerJSON = JSON.parse(body);   //Convert JSON text to a JavaScript object
-        var jsonForTemplate = processOERresponse(today, oerJSON);  // Rearrange JSON into a more useful format for display in the template
-        callback(jsonForTemplate);
-    }
+        if (!error && oer_response.statusCode == 200) {
+            //No error, and there is a response from OER. Expect the response to be a string.
+            //console.log("NASA SAYS \n" + JSON.stringify(body));
+            var oerJSON = JSON.parse(body);   //Convert JSON text to a JavaScript object
+            var jsonForTemplate = processOERresponse(today, oerJSON);  // Rearrange JSON into a more useful format for display in the template
+            callback(jsonForTemplate);
+        }
 
-    else {
-        //Log error info to console and return error with message.
-        console.log("Error in JSON request: " + error);
-        console.log(oer_response);
-        console.log(body);
-        callback(null, Error("Error fetching data"));
-    }
-});
-
+        else {
+            //Log error info to console and return error with message.
+            console.log("Error in JSON request: " + error);
+            console.log(oer_response);
+            console.log(body);
+            callback(null, Error("Error fetching data"));
+        }
+    });
+}
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,64 +1,62 @@
 var express = require('express');
 var router = express.Router();
-var oer = require('../helpers/oer');
-var OERRequest = require('oer.js');
+//retrieves the info from the API
+var OERRequest = require('../helpers/oer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Open Exchange Rates' });
-});
-
-/* GET a rate from OER service */
-router.get('/fetch_picture', function(req, res, next){
-    if (req.query.today) {
-        apod(function(data, error){
-            if (error) {
-                return res.render('apod_Error', { error : error.message });
-            }
-            return res.render('picture', { apod : data });
-        }, true);
-    }
-
-    else if (req.query.random) {
-        apod(function(data, error) {
-            if (error) {
-                return res.render('apod_error', { error : error.message });
-            }
-            return res.render('picture', { apod : data });
-        });
-
-    } else {
-        next();// Send to next route handler.
-        // Since we haven't defined one, this will end up at the 404 error handler
-        //  }});
-    }
-});
-
-
-//
-OERRequest.rates
-
-
- /* Handle GET request for home page*/
-router.get('/', function(req, res){
     res.render('index');
 });
 
-/* Handle currency form submit*/
-router.get('/convert', function(req,res){
-    var dollars = req.query.dollar_amount;
+/* GET a rate from OER service */
+router.get('/convert', function(req, res){
+   //Get the users amount
+    var input = req.query.dollar_amount;
+    //use the to and from selections
     var convertTo = req.query.to_currency;
     var convertFrom = req.query.from_currency;
+
+//use the oerrequest function to return calculations
+    OERRequest(function(err,oer){
+        //use if and else to check for error handling
+        if(err){
+            return next(err);
+        }
+        else {
+            //gets the currency code from JSON object
+            var rateTo = oer[convertTo];
+            var rateFrom = oer[convertFrom];
+
+           //Converts to USD first
+            var toDollars = input / rateFrom;
+            var result = toDollars * rateTo;
+
+            //  incorrect currency?
+            console.log(rateTo + " " + rateFrom);
+
+
+            //Renders template page with variables
+            res.render('results', {input: input, result: result, currencyTo: convertTo, currency: convertFrom})
+        }
+        });
+
+});
+
+
+
+/* Handle currency form submit
+router.get('/convert', function(req,res){
+    var dollars = req.query.dollar_amount;
+
     //res.send('To do: convert $' + dollars + ' to ' + convertTo);
    var rateTwo= exchangeRates[convertTo];
    var rate = exchangeRates[convertFrom];
     USCurrency =  dollars*rate;
     result2 = USCurrency/rateTwo;
     if(convertTo == convertFrom){
+    */
 
-    }
-    res.render('results', {dollars: dollars, UsCurrency: USCurrency, result2: result2, currency: convertFrom})
-});
+
 
 
     module.exports=router;
